33aef75d7ef7944142df91340d1cf5209404c812
AJ Javadi <amirjavadi25@gmail.com>
2025-05-18 03:59:42 -0500

    misc

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+
 
 class Post(models.Model):
     """Model representing a blog post."""
@@ -18,11 +26,18 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        """Return the absolute URL to a blog post."""
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
 
